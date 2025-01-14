4556b272cfdfaf39aaaac6d3c98842eb953bcae3
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-01-14 15:03:59 -0300

    first commit

---
+++ Example Combine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example Combine
+//
+//  Created by Rodrigo Amora on 14/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Example CombineTests/Example_CombineTests.swift
@@ -0,0 +1,17 @@
+//
+//  Example_CombineTests.swift
+//  Example CombineTests
+//
+//  Created by Rodrigo Amora on 14/01/25.
+//
+
+import Testing
+@testable import Example_Combine
+
+struct Example_CombineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
