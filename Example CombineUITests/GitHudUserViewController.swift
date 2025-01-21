377ccb7a260b0f1deafd675d888a0a60a343b85b
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-01-20 23:21:10 -0300

    UITests for search GitHubUser

---
+++ Example CombineUITests/GitHudUserViewController.swift
@@ -0,0 +1,51 @@
+//
+//  GitHudUserViewController.swift
+//  Example CombineUITests
+//
+//  Created by Rodrigo Amora on 20/01/25.
+//
+
+import Foundation
+import XCTest
+
+class GitHudUserViewController: XCTestCase {
+    
+    // MARK: - Atributes
+    var app: XCUIApplication!
+
+    // MARK: - setUp
+    override func setUp() {
+        continueAfterFailure = false
+        self.app = XCUIApplication()
+        self.app.launchArguments = ["testing"]
+        self.app.launch()
+    }
+    
+    // MARK: - tearDownWithError
+    override func tearDownWithError() throws {}
+        
+    // MARK: - Test Methods
+    func testSearchCharacter() throws {
+        let gitHubUserName = "RodrigoAmora"
+        
+        let userNameTextField = self.app.textFields["userNameTextField"]
+        userNameTextField.tap()
+        userNameTextField.typeText(gitHubUserName)
+        
+        let sendButton = self.app.buttons["sendButton"]
+        sendButton.tap()
+        
+        let expectation = XCTestExpectation(description: "Your expectation")
+        let timeInSeconds = 7.0
+        DispatchQueue.main.asyncAfter(deadline: .now() + timeInSeconds) {
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: timeInSeconds + 1.0)
+        
+        let userNameLabel = self.app.staticTexts["userNameLabel"]
+        
+        XCTAssertFalse(userNameLabel.label.isEmpty)
+        XCTAssertEqual(gitHubUserName, userNameLabel.label)
+    }
+    
+}
